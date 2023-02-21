5540f2476fbc8ed15c1cb44587fa5646147f05de
ADETOYESE Samson <adetoyesesamson@gmail.com>
2023-02-21 13:00:51 +0100

    push

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,24 @@
 import React from 'react'
 
-const Form = ({setInputText}) => {
+const Form = ({inputText, setInputText, todos, setTodos}) => {
     const inputTextHandler =(e) => {
-        console.log(e.target.value)
-        setInputText(e.target.value)
+        console.log(e.target.value);
+        setInputText(e.target.value);
     }
+    const submitTodoHandler = (e) => {
+  
+    e.preventDefault();
+    setTodos([
+        ...todos,
+         {text: inputText, completed: false, id: Math.random() * 1000 }
+    ]);
+    setInputText("");
+    };
   return (
  <form>
-    <input onChange={inputTextHandler} type="text" className='todo-input' />
-    <button className='todo-botton' type='submit'>
-        <i className='fas fa-plus-square'></i>
+    <input value={inputText} onChange={inputTextHandler} type="text" className='todo-input' />
+    <button onClick={submitTodoHandler} className='todo-botton' type='submit'>
+    <i className="fa fa-plus" aria-hidden="true"></i>
     </button>
     <div className='select'>
         <select name="todos" className='filter-todo'>
